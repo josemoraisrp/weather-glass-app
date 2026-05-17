54fd7b2a6371d8f8547ac5cc6387ca04f8db743d
José Morais <RPMORAYS@GMAIL.COM>
2026-05-17 17:08:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,55 +1,166 @@
-const botao = document.querySelector("button")
+const botao =
+    document.querySelector("button")
 
-const input = document.querySelector("#cidade")
+const input =
+    document.querySelector("#cidade")
 
+const weatherInfo =
+    document.querySelector(".weather-info")
 
-botao.addEventListener("click", buscarCidade)
+const loading =
+    document.querySelector(".loading")
 
-input.addEventListener("keypress", function(event){
+// =========================
+// EVENTOS
+// =========================
 
-  if(event.key === "Enter"){
+botao.addEventListener(
+    "click",
+    buscarCidade
+)
 
-    buscarCidade()
+input.addEventListener(
+    "keypress",
+    function(event){
 
-  }
+        if(event.key === "Enter"){
 
-})
+            buscarCidade()
+        }
+    }
+)
 
-async function buscarCidade() {
-    document.querySelector(".loading").innerHTML =
-  "Carregando..."
+// =========================
+// BUSCAR CIDADE
+// =========================
 
-  const cidade = document.querySelector("#cidade").value
+async function buscarCidade(){
 
-  const chave = "9b1143a7aacc99cba3d02c7319c1233d"
+    const cidade =
+        document.querySelector("#cidade").value
 
-  const dados = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`
-  ).then(resposta => resposta.json())
+    // VALIDAÇÃO
 
-  console.log(dados)
+    if(cidade === ""){
 
-  document.querySelector(".loading").innerHTML = ""
+        loading.innerHTML =
+            "Digite uma cidade"
 
-  document.querySelector(".cidade").innerHTML = dados.name
+        return
+    }
 
-document.querySelector(".temp").innerHTML =
-  Math.floor(dados.main.temp) + "°C"
+    // LOADING
 
-document.querySelector(".description").innerHTML =
-  dados.weather[0].description
+    loading.innerHTML =
+        "Carregando..."
 
-  const icone = dados.weather[0].icon
+    weatherInfo.classList.add(
+        "loading-weather"
+    )
 
-  const clima = dados.weather[0].main
+    botao.innerHTML =
+        "Buscando..."
 
-document.querySelector(".icone").src =
-  `https://openweathermap.org/img/wn/${icone}@2x.png`
+    // API
 
-document.querySelector(".umidade").innerHTML =
-  "💧 Umidade: " + dados.main.humidity + "%"
+    const chave =
+        "9b1143a7aacc99cba3d02c7319c1233d"
 
-document.querySelector(".vento").innerHTML =
-  "🌬️ Vento: " + dados.wind.speed + " km/h"
+    try{
+
+        const dados = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${chave}&lang=pt_br&units=metric`
+        ).then(
+            resposta => resposta.json()
+        )
+
+        // CIDADE NÃO ENCONTRADA
+
+        if(dados.cod == "404"){
+
+            loading.innerHTML =
+                "Cidade não encontrada"
+
+            weatherInfo.classList.remove(
+                "loading-weather"
+            )
+
+            botao.innerHTML =
+                "Buscar"
+
+            return
+        }
+
+        // LIMPA LOADING
+
+        loading.innerHTML = ""
+
+        // CIDADE
+
+        document.querySelector(".cidade")
+        .innerHTML =
+            dados.name
+
+        // TEMPERATURA
+
+        document.querySelector(".temp")
+        .innerHTML =
+            Math.floor(
+                dados.main.temp
+            ) + "°C"
+
+        // DESCRIÇÃO
+
+        document.querySelector(".description")
+        .innerHTML =
+            dados.weather[0].description
+
+        // ÍCONE
+
+        const icone =
+            dados.weather[0].icon
+
+        document.querySelector(".icone")
+        .src =
+            `https://openweathermap.org/img/wn/${icone}@2x.png`
+
+        // UMIDADE
+
+        document.querySelector(".umidade")
+        .innerHTML =
+            "💧 Umidade: " +
+            dados.main.humidity + "%"
+
+        // VENTO
+
+        document.querySelector(".vento")
+        .innerHTML =
+            "🌬️ Vento: " +
+            dados.wind.speed + " km/h"
+
+        // ANIMAÇÃO
+
+        weatherInfo.classList.remove(
+            "loading-weather"
+        )
+
+        // BOTÃO
+
+        botao.innerHTML =
+            "Buscar"
+
+    }catch(erro){
+
+        loading.innerHTML =
+            "Erro ao buscar cidade"
+
+        botao.innerHTML =
+            "Buscar"
+
+        weatherInfo.classList.remove(
+            "loading-weather"
+        )
+
+        console.log(erro)
+    }
 }
-
